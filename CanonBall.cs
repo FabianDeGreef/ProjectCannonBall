b5913b0dc8bf76d520b9d45cf05aaccffbca2b8b
Fabian <fabian.degreef@student.pxl.be>
2017-04-12 15:08:01 +0200

    Ball Value Match
    Fixed Ball Trajectory Bug

--- CanonBall.cs
+++ CanonBall.cs
@@ -13,12 +13,59 @@ namespace Oef10_0KogelBaan
     public class CanonBall
     {
         private Canvas world;
-        private int size = 20;
+        private int size = 15;
         private Ellipse canonBall;
 
         public CanonBall(Canvas world)
         {
             this.world = world;
+        }
+
+        public void Update(int meterSeconden, int angle, int seconden)
+        {
+            // Schaal
+            meterSeconden = meterSeconden * 5;
+
+            // Radialen
+            double radialen = Math.PI * angle / 180;
+
+            // Horizontale afgelegde afstand in meters
+            double horizontalMeter = meterSeconden * Math.Cos(radialen) * seconden;
+
+            // Veritcaal afgelegde afstand in meters
+            double verticalMeter = (meterSeconden * Math.Sin(radialen) * seconden) - (9.81 * (meterSeconden / (Math.Pow(seconden, 2)))) / 2 * Math.Pow(seconden, 2);
+
+
+            canonBall.Margin = new Thickness(horizontalMeter, verticalMeter + world.Height, 0, 0);
+
+            //canonBall.Margin = new Thickness(horizontalMeter+canonBall.Margin.Left, canonBall.Margin.Top + (verticalMeter), 0, 0);
+        }
+        public void GroundHit()
+        {
+            canonBall.Margin = new Thickness(canonBall.Margin.Left, world.Height - size, 0, 0);
+        }
+
+        public double With
+        {
+            get
+            {
+                return canonBall.Margin.Left + size / 2;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return -canonBall.Margin.Top + (world.Height - 80);
+            }
+        }
+        public double Size
+        {
+            get
+            {
+                return size;
+            }
         }
 
         public void drawCanonBall()
@@ -28,45 +75,8 @@ namespace Oef10_0KogelBaan
             canonBall.Width = size;
             canonBall.Stroke = new SolidColorBrush(Colors.Red);
             canonBall.Fill = new SolidColorBrush(Colors.Red);
-            canonBall.Margin = new Thickness(0, world.Height - 80, 0, 0);
+            canonBall.Margin = new Thickness(0, world.Height - 85, 0, 0);
             world.Children.Add(canonBall);
-        }
-        public void Update(int meterSeconden, int seconden, int angle)
-        {
-            meterSeconden = meterSeconden * 5;
-            double radialen = (angle * Math.PI) / 180;
-
-            // Horizontale afgelegde afstand in meters
-            double horizontalMeter = meterSeconden * Math.Cos(radialen) * seconden;
-
-            // Veritcaal afgelegde afstand in meters
-            double verticalMeter = (meterSeconden * Math.Sin(radialen) * seconden) - (9.81 * (meterSeconden / (Math.Pow(seconden, 2))))/2* Math.Pow(seconden, 2);
-
-            canonBall.Margin = new Thickness(horizontalMeter+canonBall.Margin.Left, canonBall.Margin.Top + (verticalMeter), 0, 0);
-        }
-
-        public void OutDistance()
-        {
-                canonBall.Margin = new Thickness(1500-size, 600-size, 0, 0);
-        }
-        public void GroundHit()
-        {
-            canonBall.Margin = new Thickness(canonBall.Margin.Left - size, 600 - size, 0, 0);
-        }
-
-        public double With
-        { get
-            {
-                return canonBall.Margin.Left;
-            }
-        }
-
-        public double Height
-        {
-            get
-            {
-                return -canonBall.Margin.Top+600+80;
-            }
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,30 +28,26 @@ namespace Oef10_0KogelBaan
         public MainWindow()
         {
             InitializeComponent();
+            canonball = new CanonBall(canvasWorld);
             labelAngle.Content = Convert.ToString((int)sliderAngle.Value);
             labelSpeed.Content = Convert.ToString((int)sliderSpeed.Value);
             world = new World(canvasWorld);
             world.drawMeters();
             timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(50);
+            timer.Interval = TimeSpan.FromMilliseconds(400);
             timer.Tick += Timer_Tick;
-            canonball = new CanonBall(canvasWorld);
+
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
             seconden++;
-            canonball.Update((int)sliderSpeed.Value, seconden, (int)sliderAngle.Value);
+            canonball.Update((int)sliderSpeed.Value, (int)sliderAngle.Value, seconden);
+
             labelDistanceMeter.Content = String.Format("{0:0.00}", canonball.With / 5);
             labelHeightMeter.Content = String.Format("{0:0.00}", canonball.Height / 5);
-            if (canonball.With / 5 >= 300)
-            {
-                timer.Stop();
-                canonball.OutDistance();
-                labelDistanceMeter.Content = String.Format("{0:0.00}", 300);
-                labelHeightMeter.Content = String.Format("{0:0.00}", 0);
-            }
-            else if (canonball.Height / 5 <= 0)
+
+            if (canonball.Height / 5 <= canonball.Size)
             {
                 timer.Stop();
                 canonball.GroundHit();
@@ -65,6 +61,7 @@ namespace Oef10_0KogelBaan
             canonball.drawCanonBall();
             sliderAngle.IsEnabled = false;
             sliderSpeed.IsEnabled = false;
+            buttonShoot.IsEnabled = false;
         }
 
         private void sliderSpeed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
